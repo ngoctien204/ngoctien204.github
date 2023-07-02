acbd223e858e2878f4908b720aab3c83104900b0
ngoctien204 <132563170+ngoctien204@users.noreply.github.com>
2023-07-02 09:58:02 +0700

    Add files via upload

---
+++ datamaytinh.js
@@ -0,0 +1,176 @@
+
+// Khai báo 1 mảng danh sách lap
+// Cần khai báo 20 bản ghi
+const DanhSachMayTinh = [
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    {
+      id: 1,
+      tenMayTinh: 'Dell Precision 5530 i7-8850H/ RAM 16GB/ SSD 512GB/ P1000/ 15.6 INCH FHD',
+      hang: 'Dell',
+      soluong: 10,
+      ngaynhap: '01-07-2023',
+      hinhanh: 'https://ngocnguyen.vn/cdn/images/202304/goods_img/dell-precision-5530-i7-8850h-ram-8gb-ssd-256gb-p1000-156-inch-fhd-G7204-1681203137115.jpg'
+    },
+    
+];
+    // Sử dụng dữ liệu danh sách lap
+    DanhSachMayTinh.forEach((MayTinh) => {
+      console.log('ID:', MayTinh.id);
+      console.log('Tên Máy Tính:', MayTinh.tenMayTinh);
+      console.log('Hãng:', MayTinh.hang);
+      console.log('Số Lượng:', MayTinh.soluong);
+      console.log('Ngày Nhập:', MayTinh.ngaynhap);
+      console.log('Hình ảnh :', MayTinh.hinhanh);
+      console.log('--------------------------------');
+    });
